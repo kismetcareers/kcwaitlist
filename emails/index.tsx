ac64a331e7380a161f6285c9e60055ee2730a150
kismetcareers <kismetcareers@gmail.com>
2025-03-24 14:54:33 -0400

    Update index.tsx

--- emails/index.tsx
+++ emails/index.tsx
@@ -29,33 +29,31 @@ export const NotionWaitlistEmail = ({ userFirstname }: EmailProps) => (
         />
         <Text style={greeting}>Hi {userFirstname},</Text>
         <Text style={paragraph}>
-          Thanks for joining the waitlist for our Next.js + Notion CMS waitlist
-          template! I'm Lakshay, the developer behind this project. I'm glad to
-          have you on board.
+          Thanks for joining the waitlist for our Job Application Submission service!
         </Text>
         <Text style={paragraph}>
-          I'll keep you posted on the progress and notify you as soon as it's
+          We'll keep you posted on the progress and notify you as soon as it's
           ready for you to use. In the meantime, if you have any questions or
           feedback, don't hesitate to reach out by replying directly to{" "}
-          <a href="mailto:lakshb.work@gmail.com" style={link}>
+          <a href="mailto:kismetcareers@gmail.com" style={link}>
             this email {""}
           </a>
-          — I'm here to listen!
+          — We're here to help!
         </Text>
         <Text style={paragraph}>
-          You can also follow me on X/Twitter for updates:{" "}
-          <a href="https://x.com/blakssh" style={link}>
-            @blakssh
+          You can also follow us on TikTok for updates:{" "}
+          <a href="https://www.tiktok.com/@kismetcareers" style={link}>
+            @kismetcareers
           </a>
         </Text>
         <Text style={signOff}>
           Best regards,
           <br />
-          Lakshay
+          The Kismet Careers Team
         </Text>
         <Hr style={hr} />
         <Text style={footer}>
-          You received this email because you signed up for the Notion waitlist.
+          You received this email because you signed up for the Kismet Careers waitlist.
           If you believe this is a mistake, feel free to ignore this email.
         </Text>
       </Container>
