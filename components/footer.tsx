8114f15e8e21350590dfc02f3e37a6707b1cb427
kismetcareers <kismetcareers@gmail.com>
2025-03-24 14:17:49 -0400

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,13 +10,13 @@ export default function Footer() {
       animate="visible"
       className="mt-auto flex w-full items-center justify-center gap-1 border-t bg-background p-6 text-muted-foreground md:justify-start">
       <motion.div variants={itemVariants}>
-        Guiding you to the career that was always yours{" "}
+        Kismet Careers - Guiding you to the career that was always yours{" "}
         <Link
           href="https://waitlist.kismetcareers.com"
           rel="noopener noreferrer"
           target="_blank">
           <span className="text-zinc-300 underline underline-offset-2 transition-all duration-200 ease-linear hover:text-yellow-200">
-            Kismet Careers
+            Sign up for your new career
           </span>
           .
         </Link>
