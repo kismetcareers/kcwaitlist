fb386a9bbd4781fb22a4025fe8a781a20da91156
kismetcareers <kismetcareers@gmail.com>
2025-03-24 15:20:59 -0400

    Update logos.tsx

--- components/logos.tsx
+++ components/logos.tsx
@@ -35,26 +35,7 @@ export default function Logos() {
         />
       </motion.div>
 
-     /* <motion.div
-        variants={itemVariants}
-        className="mt-4 grid w-full grid-cols-2 items-center justify-center gap-4 md:mt-6 md:grid-cols-3 md:gap-6">
-        {logos.map((logo, index) => (
-          <Link
-            key={index}
-            href={logo.href}
-            rel="noopener noreferrer"
-            target="_blank"
-            className="flex h-24 items-center justify-center rounded-lg border bg-zinc-900 p-8 transition-all duration-150 ease-in-out md:hover:border-zinc-700 md:hover:bg-accent">
-            <Image
-              src={logo.src}
-              alt={logo.alt}
-              width={100}
-              height={100}
-              className="h-auto w-32 opacity-85"
-            />
-          </Link>
-        ))}
-      </motion.div> */
+     
     </motion.div>
   ); 
 }
